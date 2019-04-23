94eb663a9894cbc970e72256a3f4463c899d916c
284582220 <284582220>
2019-04-23 10:15:56 +0800

    init java-app project

---
+++ src/main/java/com/yangguojun/App.java
@@ -0,0 +1,15 @@
+package com.yangguojun;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String args[]) {
+
+        log.debug("hello");
+
+    }
+}
